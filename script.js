6ed5d9c6aa0017139c1f926af51e0e5714d777f4
Priyal Dahiya <140418518+priyaldahiya@users.noreply.github.com>
2023-08-17 01:42:38 -0400

    Intro to JS, CSS, HTML Project
    Completed IBM Full Stack Developer Certificate's intro to JS, CSS, HTML final project. A portfolio website.

---
+++ script.js
@@ -0,0 +1,38 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    showPopup(true);
+    //Call showPopup here
+
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Get a reference to the button element
+    var recommendButton = document.getElementById("recommend_btn");
+
+    // Add an onClick event listener
+    recommendButton.addEventListener("click", function() {
+        // Code to be executed when the button is clicked
+        alert("Button clicked!");
+    });
+
+
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
